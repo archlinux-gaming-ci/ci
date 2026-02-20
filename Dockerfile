27253cdb742321514a08d7459b244423ac868db5
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2026-02-20 21:32:37 +0100

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM archlinux:latest
+FROM archlinux:latest AS builder
 
 RUN useradd -m builduser
 
@@ -15,3 +15,7 @@ RUN git clone https://aur.archlinux.org/${PACKAGE_NAME}.git /tmp/package
 WORKDIR /tmp/package
 
 RUN makepkg -s --noconfirm
+
+FROM scratch
+
+COPY --from=builder /tmp/package/*.pkg.tar.zst /
--- docker-build-package.sh
+++ docker-build-package.sh
@@ -11,10 +11,18 @@ function cleanup() {
 trap cleanup EXIT
 
 package=$1
+output_dir=$2
 
-if [[ -z "$package" ]]; then
-    echo "expected $0 <package>"
+if [[ -z "${package}" ]]; then
+    echo "expected $0 <package> <output-dir>"
     exit 1
 fi
 
-docker buildx build --build-arg PACKAGE_NAME="${package}" "${PLATFORM_ARGS[@]}" --output type=local,dest="$tmpdir" --progress plain .
+if [[ -z "${output_dir}" ]]; then
+    echo "expected $0 ${package} <output-dir>"
+    exit 1
+fi
+
+docker buildx build --build-arg PACKAGE_NAME="${package}" --output type=local,dest="${tmpdir}" --progress plain .
+
+cp -p "${tmpdir}"/*.pkg.tar.zst "${output_dir}"/
